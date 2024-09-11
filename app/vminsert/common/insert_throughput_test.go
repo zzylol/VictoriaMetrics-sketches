deaa907617e2b42abe6f60a600639ef2d35770c2
Zeying Zhu <zeyingz@umd.edu>
2024-09-11 17:26:23 -0400

    update

--- app/vminsert/common/insert_throughput_test.go
+++ app/vminsert/common/insert_throughput_test.go
@@ -46,27 +46,22 @@ func TestWriteZipfThroughPut(t *testing.T) {
 		}
 		mr := &metricRows[ts_id]
 		mr.MetricNameRaw = mn.MarshalRaw(mr.MetricNameRaw[:0])
-		/*
-			err := vmsketch.RegisterMetricNameFuncName(&mn, "distinct_over_time", 100000000, 1000000)
-			if err != nil {
-				panic(fmt.Errorf("Failed register vmsketch cache EHuniv instance %w", err))
-			}
-		*/
-		err := vmsketch.RegisterMetricNameFuncName(&mn, "avg_over_time", 1000000, 10000)
+
+		err := vmsketch.RegisterMetricNameFuncName(&mn, "distinct_over_time", 10000000, 100000)
 		if err != nil {
 			panic(fmt.Errorf("Failed register vmsketch cache EHuniv instance %w", err))
 		}
-		/*
-				err = vmsketch.RegisterMetricNameFuncName(&mn, "avg_over_time", 100000000, 1000000)
-				if err != nil {
-					panic(fmt.Errorf("Failed register vmsketch cache Sampling instance %w", err))
-				}
 
-			err := vmsketch.RegisterMetricNameFuncName(&mn, "quantile_over_time", 10000000, 100000)
-			if err != nil {
-				panic(fmt.Errorf("Failed register vmsketch cache EHKLL instance %w", err))
-			}
-		*/
+		err = vmsketch.RegisterMetricNameFuncName(&mn, "avg_over_time", 10000000, 100000)
+		if err != nil {
+			panic(fmt.Errorf("Failed register vmsketch cache Sampling instance %w", err))
+		}
+
+		err = vmsketch.RegisterMetricNameFuncName(&mn, "quantile_over_time", 10000000, 100000)
+		if err != nil {
+			panic(fmt.Errorf("Failed register vmsketch cache EHKLL instance %w", err))
+		}
+
 	}
 
 	tNow := time.Now()
@@ -104,7 +99,7 @@ func ingestZipfScrapes(st *storage.Storage, mrs []storage.MetricRow, scrapeTotCo
 				z := rand.NewZipf(RAND, s, v, uint64(100000))
 
 				for j := 0; j < scrapeBatch; j++ {
-					var wg_sketch sync.WaitGroup
+					// var wg_sketch sync.WaitGroup
 					rowsToInsert := make([]storage.MetricRow, 0, len(batch))
 					ts := int64(j*second) + currTime
 					for _, mr := range batch {
@@ -113,18 +108,21 @@ func ingestZipfScrapes(st *storage.Storage, mrs []storage.MetricRow, scrapeTotCo
 						rowsToInsert = append(rowsToInsert, mr)
 					}
 
-					wg_sketch.Add(1)
-					go func(rowsToInsert []storage.MetricRow) {
-						defer wg_sketch.Done()
-						for _, mr := range rowsToInsert {
-							vmsketch.AddRow(mr.MetricNameRaw, mr.Timestamp, mr.Value)
-						}
-					}(rowsToInsert)
-
-					if err := st.AddRows(rowsToInsert, defaultPrecisionBits); err != nil {
-						panic(fmt.Errorf("cannot add rows to storage: %w", err))
+					// wg_sketch.Add(1)
+					// go func(rowsToInsert []storage.MetricRow) {
+					// 	defer wg_sketch.Done()
+					for _, mr := range rowsToInsert {
+						vmsketch.AddRow(mr.MetricNameRaw, mr.Timestamp, mr.Value)
 					}
-					wg_sketch.Wait()
+					// }(rowsToInsert)
+
+					/*
+						if err := st.AddRows(rowsToInsert, defaultPrecisionBits); err != nil {
+							panic(fmt.Errorf("cannot add rows to storage: %w", err))
+						}
+					*/
+
+					// wg_sketch.Wait()
 				}
 
 			}(currTime)
