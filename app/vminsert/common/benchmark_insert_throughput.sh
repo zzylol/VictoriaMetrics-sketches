0699c783ee3f2de2af7ce041a9ba8d74bc68efc5
Zeying Zhu <zeyingz@umd.edu>
2024-09-23 22:19:56 -0400

    update

--- app/vminsert/common/benchmark_insert_throughput.sh
+++ app/vminsert/common/benchmark_insert_throughput.sh
@@ -9,5 +9,5 @@ go test -v -count 5 -timeout 0 -run ^TestWriteZipfThroughPut$ github.com/zzylol/
 rm -r BenchmarkCtxInsertThoughput/
 go test -v -count 5 -timeout 0 -run ^TestWriteZipfThroughPut$ github.com/zzylol/VictoriaMetrics-sketches/app/vminsert/common -numts=10000
 rm -r BenchmarkCtxInsertThoughput/
-go test -v -count 5 -timeout 0 -run ^TestWriteZipfThroughPut$ github.com/zzylol/VictoriaMetrics-sketches/app/vminsert/common -numts=100000
+# go test -v -count 5 -timeout 0 -run ^TestWriteZipfThroughPut$ github.com/zzylol/VictoriaMetrics-sketches/app/vminsert/common -numts=100000
 rm -r BenchmarkCtxInsertThoughput/
